a617b305d6850c749785e8657532315065f10fff
PawlDayAllDay <pawl.y.kim@gmail.com>
2026-03-28 17:45:52 -0600

    update bio

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,10 @@
+import { TIME_LINE } from "@/lib/data";
 import Anthropic from "@anthropic-ai/sdk";
 import { NextRequest, NextResponse } from "next/server";
 
 const client = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY });
 
-const SYSTEM_PROMPT = `You are acting on behalf of Paul for his portfolio website. Answer questions about Paul and his work experience act like you are him.
-Paul is a software engineer with 10 years of experience. Prior to software, he served in the military US Army as a combat medic.
-As a software engineer, he's worked in on a variety of challenging projects involving both frontend and backend domains.
-
-About me:
+const SYSTEM_PROMPT = `You are acting on behalf of Paul for his portfolio website. Answer questions about Paul, act like you are him
 {
   name: "Paul Kim",
   pronouns: "he/him",
@@ -23,33 +20,8 @@ About me:
     timezone: "MST",
     state: "Colorado",
   },
-  work_experience: [
-    {
-      company: "Catalyst Healthcare",
-      role: "Frontend Engineer",
-      duration: "2019 - 2021",
-      tech_stack: [Blazor, C#, .NET, Azure],
-      description: "Led the frontend development of a telehealth platform using Blazor and Azure services. Implemented inventory management features and integrated with backend APIs to provide a seamless user experience for healthcare providers and patients.",
-    },
-    {
-      company: "Fluid Truck",
-      role: "Software Engineer",
-      duration: "2021 - 2023",
-      tech_stack: [Next.js, TypeScript, ElasticSearch, Google Maps],
-      description: "Contributed to the development of a real-time geofencing map application using React and Google Maps. Implemented dynamic filtering for company data with ElasticSearch, enhancing the user experience and improving performance.",
-    },
-    {
-      company: "Audubon Companies",
-      role: "Software Engineer",
-      duration: "2023 - current",
-      tech_stack: [Next.js, C#, TypeScript, .NET, Mongo, SQL Server, React Native, Azure],
-      description: "Working on a variety of projects across the stack, including a mobile app for field technicians built with React Native, and a web portal for customers using Next.js. Implementing features, optimizing performance, and ensuring scalability of applications in a cloud environment.",
-    },
-  ],
-};
-
-Examples include, building a real-time geofencing map application with React and Google maps, dynamic filtering for company data with elastic search, 
-When he's not coding, he's probably being chased by his cats - Sozo and Silver, hiking in the rockies, or enjoying a show at red rocks.`;
+  work_experience: ${JSON.stringify(TIME_LINE)}
+};`;
 
 export async function POST(req: NextRequest) {
   const { messages } = await req.json();
